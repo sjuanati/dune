c03f1a4a85d0f66687c7fecf3d9e9c381a8acf98
Sergi Juanati <wint3rmute@gmail.com>
2022-08-02 18:53:14 +0200

    Dashboard <GRO - TVL acquisition, Retention, Churn> updated to v3.0

--- 1. GRO - TVL acquisition, Retention, Churn/GVT - TVL acquisition, Retention, Churn.sql
+++ 1. GRO - TVL acquisition, Retention, Churn/GVT - TVL acquisition, Retention, Churn.sql
@@ -4,79 +4,106 @@
 ///         - Renewed TVL: Deposits into the protocol during period by wallets that had deposited into the protocol before
 ///         - Churned TVL: Withdrawals from the protocol during this period
 ///         - Retained: TVL in the protocol during the previous time period deducting churned TVL from this period
-/// @dev    - TVL based on deposit & withdrawal handler events, therefore excluding returns
+///         - Yield: TVL based on the diff between previous TVL and current retained TVL + churned TVL 
+/// @dev    - PWRD: TVL based on deposit & withdrawal handler events, therefore excluding returns
+///         - GVT: TVL based on transfer & PNL events, therefore including returns
 */
 
--- version 2.0
+-- version 3.0 [GVT now based on realised TVL]
 WITH
     -- Time period for the Dashboard (must be a week date, i.e.: 2021-05-24, 2021-05-31...)
     period AS (
         SELECT generate_series('2021-05-24'::TIMESTAMP, date_trunc('week', NOW()), '1 week') AS "week"
     ),
-    -- Deposits & withdrawals
+    -- Total GVT supply based on ERC20 transfers
+    gvt_total_supply AS (
+        SELECT week,
+            SUM(COALESCE("total_supply", 0)) OVER (ORDER BY week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) AS "total_supply"
+        FROM (
+                SELECT
+                    date_trunc('week', "evt_block_time") AS "week", 
+                    SUM(
+                        CASE
+                            WHEN "from" = '\x0000000000000000000000000000000000000000' THEN value
+                            WHEN "to" = '\x0000000000000000000000000000000000000000' THEN -value
+                        END
+                    ) / 1e18 AS "total_supply"
+                FROM erc20."ERC20_evt_Transfer" tr
+                WHERE contract_address = '\x3ADb04E127b9C0a5D36094125669d4603AC52a0c'
+                GROUP BY 1
+                ORDER BY 1 DESC
+        ) gvt_supply
+    ),
+    -- GVT deposits & withdrawals based on ERC20 transfers
     gvt AS (
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
+        SELECT date_trunc('week', "evt_block_time") AS "week", 
+               "value" / 1e18 AS "amount",
                'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandler_evt_LogNewDeposit"
-         WHERE pwrd = false
+               "to" AS "wallet"
+            FROM erc20."ERC20_evt_Transfer" tr
+            WHERE contract_address = '\x3ADb04E127b9C0a5D36094125669d4603AC52a0c'
+              AND "from" = '\x0000000000000000000000000000000000000000'
         UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
+        SELECT date_trunc('week', "evt_block_time") AS "week", 
+               "value" / 1e18 AS "amount",
                'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandler_evt_LogNewWithdrawal"
-         WHERE pwrd = false
-         UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
-               'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandlerPrev1_evt_LogNewDeposit"
-         WHERE pwrd = false
-        UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
-               'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandlerPrev1_evt_LogNewWithdrawal"
-         WHERE pwrd = false
-         UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
-               'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandlerPrev2_evt_LogNewDeposit"
-         WHERE pwrd = false
-        UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
-               'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandlerPrev2_evt_LogNewWithdrawal"
-         WHERE pwrd = false
+               "from" AS "wallet"
+            FROM erc20."ERC20_evt_Transfer" tr
+            WHERE contract_address = '\x3ADb04E127b9C0a5D36094125669d4603AC52a0c'
+              AND "to" = '\x0000000000000000000000000000000000000000'
     ),
     -- Sum of deposits & withdrawals by week & wallet
     gvt_acc AS (
-        SELECT week, 
-               wallet, 
-               SUM(CASE WHEN type = 'deposit' THEN gvt.amount END) as deposit,
-               SUM(CASE WHEN type = 'withdrawal' THEN gvt.amount END) as withdraw
+        SELECT week AS "week", 
+               wallet AS "wallet", 
+               SUM(CASE WHEN type = 'deposit' THEN gvt.amount END) AS "deposit",
+               SUM(CASE WHEN type = 'withdrawal' THEN gvt.amount END) AS "withdraw"
           FROM gvt
         GROUP BY 1,2
+    ),
+    -- Latest day of a week with a PnL event
+    pnl_dates AS (
+        SELECT date_trunc('week', "evt_block_time") AS "week", 
+            max("evt_block_time") AS "max_time" 
+        FROM gro."PnL_evt_LogPnLExecution"
+        GROUP BY 1
+    ),
+    -- Utilisation Ratio & GVT totalAssets from PnL
+    pnl AS (
+        SELECT "week" AS "week",
+               max("util") AS "Utilisation Ratio",
+               max("gvt_assets") AS "gvt_total_assets"
+        FROM (
+            SELECT pnl_dates.week AS "week",
+                   pnl."afterGvtAssets" / 1e18 AS "gvt_assets",
+                   pnl."afterPwrdAssets" / pnl."afterGvtAssets" AS "util"
+            FROM gro."PnL_evt_LogPnLExecution" pnl
+            INNER JOIN pnl_dates pnl_dates
+                 ON pnl.evt_block_time = pnl_dates.max_time
+        ) pnl
+        GROUP BY 1
+    -- Current GVT price based on totalAssets / totalSupply from PnL
+    ), gvt_price AS (
+        SELECT p.week AS "week",
+               pnl.gvt_total_assets / ts.total_supply AS "gvt_price"
+          FROM period p
+          LEFT JOIN pnl pnl
+            ON p.week = pnl.week
+          LEFT JOIN gvt_total_supply ts
+            on p.week = ts.week
+    -- GVT price on previous week based on totalAssets / totalSupply from PnL
+    ), gvt_price_7d AS (
+        SELECT current.week AS "week",
+               prev.gvt_price AS "gvt_price_7d"
+          FROM gvt_price current,
+               gvt_price prev
+         WHERE current.week - interval '7 days' = prev.week 
     ),
     -- Calculation of
     -- a) New TVL (deposits into the protocol during this period by wallets that had never deposited into the protocol before)
     -- b) Renewed TVL (deposits into the protocol during this period by wallets that had deposited into the protocol before)
     -- c) Churned TVL (withdrawals from the protocol during this period)
-    kpis AS (
+    kpis_a AS (
         SELECT week,
                sum(new_tvl) as new_tvl,
                sum(renewed_tvl) as renewed_tvl,
@@ -108,211 +135,64 @@ WITH
          ) new
          GROUP BY 1
     ),
-    -- Calculation of retained TVL (TVL in the protocol during the previous time period deducting churned TVL from this period)
-    retained_tvl AS (
+    -- Calculation of
+    -- a) Retained TVL: TVL in the protocol during the previous time period deducting churned TVL from this period
+    -- b) Previous TVL TVL on previous time period (to be used for the yield calculation)
+    kpis_b AS (
         SELECT week,
-           sum(retained_tvl) as retained_tvl
+           sum(retained_tvl) as retained_tvl,
+           sum(prev_tvl) as prev_tvl
          FROM (
             SELECT period.week,
                   -COALESCE(new_tvl,0)
                   -COALESCE(renewed_tvl, 0)
-                  +SUM(COALESCE(new_tvl, 0) + COALESCE(renewed_tvl, 0) - COALESCE(churned_tvl, 0)) OVER (ORDER BY period.week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) as "retained_tvl"
+                  +SUM(COALESCE(new_tvl, 0) + COALESCE(renewed_tvl, 0) - COALESCE(churned_tvl, 0)) OVER (ORDER BY period.week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) AS "retained_tvl",
+                  -COALESCE(new_tvl,0)
+                  -COALESCE(renewed_tvl, 0)
+                  +COALESCE(churned_tvl, 0)
+                  +SUM(COALESCE(new_tvl, 0) + COALESCE(renewed_tvl, 0) - COALESCE(churned_tvl, 0)) OVER (ORDER BY period.week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) AS "prev_tvl"
             FROM period
-            LEFT JOIN kpis
-                ON period.week = kpis.week
+            LEFT JOIN kpis_a
+                ON period.week = kpis_a.week
+            LEFT JOIN gvt_price
+                ON period.week = gvt_price.week
         ) ret
          GROUP BY 1
     ),
-    pnl_dates AS (
-        SELECT date_trunc('week', "evt_block_time") AS "week", 
-            max("evt_block_time") AS "max_time" 
-        FROM gro."PnL_evt_LogPnLExecution"
-        GROUP BY 1
-    ),
-    util_ratio AS (
-        SELECT "week" AS "week",
-               max("util") AS "Utilisation Ratio"
-        FROM (
-            SELECT pnl_dates.week AS "week",
-                   pnl."afterGvtAssets" / 1e18 as "gvt_tvl",
-                   pnl."afterPwrdAssets" / 1e18 as "pwrd_tvl",
-                   pnl."afterGvtAssets" / 1e18 + pnl."afterPwrdAssets" / 1e18 as "total_tvl",
-                   pnl."afterPwrdAssets" / pnl."afterGvtAssets" as "util"
-            FROM gro."PnL_evt_LogPnLExecution" pnl
-            INNER JOIN pnl_dates pnl_dates
-                 ON pnl.evt_block_time = pnl_dates.max_time
-        ) pnl
-        GROUP BY 1
+    -- Calculation of Yield: diff between [retained TVL + churned TVL] on current period (at current GVT price) 
+    --                       and [Total TVL] on previous period (at GVT price on previous period)
+    yield AS (
+        SELECT p.week,
+               kpis_b.retained_tvl * gvt_price.gvt_price + kpis_a.churned_tvl * gvt_price.gvt_price - kpis_b.prev_tvl * gvt_price_7d.gvt_price_7d  AS "yield"
+          FROM period p
+          LEFT JOIN kpis_b
+            ON p.week = kpis_b.week
+          LEFT JOIN gvt_price
+            ON p.week = gvt_price.week
+          LEFT JOIN gvt_price_7d
+            ON p.week = gvt_price_7d.week
+          LEFT JOIN kpis_a
+            ON p.week = kpis_a.week
     )
 
 SELECT p.week::date AS "week",
-    kpis.new_tvl AS "New TVL",
-    kpis.renewed_tvl AS "Renewed TVL",
-    -kpis.churned_tvl AS "Churned TVL",
-    rt.retained_tvl AS "Retained TVL",
-    SUM(COALESCE(kpis.new_tvl, 0) + COALESCE(kpis.renewed_tvl) - COALESCE(kpis.churned_tvl,0)) OVER (ORDER BY p.week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) AS "Total TVL",
-    ur."Utilisation Ratio" AS "Utilisation Ratio"
+    kpis_a.new_tvl * gvt_price AS "New TVL",
+    kpis_a.renewed_tvl * gvt_price AS "Renewed TVL",
+    -kpis_a.churned_tvl * gvt_price AS "Churned TVL",
+    kpis_b.retained_tvl * gvt_price AS "Retained TVL",
+    yield.yield AS "Yield",
+    SUM(COALESCE(kpis_a.new_tvl, 0) + COALESCE(kpis_a.renewed_tvl) - COALESCE(kpis_a.churned_tvl,0)) OVER (ORDER BY p.week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW) * gvt_price AS "Total TVL",
+    pnl."Utilisation Ratio" AS "Utilisation Ratio",
+    gvt_price.gvt_price
 FROM period p
-LEFT JOIN kpis
-    ON p.week = kpis.week
-LEFT JOIN retained_tvl rt
-    ON p.week = rt.week
-LEFT JOIN util_ratio ur
-    ON p.week = ur.week
+LEFT JOIN kpis_a
+    ON p.week = kpis_a.week
+LEFT JOIN kpis_b
+    ON p.week = kpis_b.week
+LEFT JOIN yield
+    ON p.week = yield.week
+LEFT JOIN pnl pnl
+    ON p.week = pnl.week
+LEFT JOIN gvt_price gvt_price
+    ON p.week = gvt_price.week
 ORDER BY 1 DESC
-
-
--- version 1.0
-/*
-WITH
-    -- Time period for the Dashboard (must be a week date, i.e.: 2021-05-24, 2021-05-31...)
-    period AS (
-        SELECT generate_series('2021-05-24'::TIMESTAMP, date_trunc('week', NOW()), '1 week') AS "week"
-    ),
-    -- Deposits & withdrawals
-    gvt AS (
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
-               'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandler_evt_LogNewDeposit"
-         WHERE pwrd = false
-        UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
-               'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandler_evt_LogNewWithdrawal"
-         WHERE pwrd = false
-         UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
-               'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandlerPrev1_evt_LogNewDeposit"
-         WHERE pwrd = false
-        UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
-               'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandlerPrev1_evt_LogNewWithdrawal"
-         WHERE pwrd = false
-         UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
-               "usdAmount" / 1e18 AS "amount",
-               'deposit' AS "type",
-               "user" AS "wallet"
-          FROM gro."DepositHandlerPrev2_evt_LogNewDeposit"
-         WHERE pwrd = false
-        UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
-               "returnUsd" / 1e18 AS "amount",
-               'withdrawal' AS "type",
-               "user" AS "wallet"
-          FROM gro."WithdrawHandlerPrev2_evt_LogNewWithdrawal"
-         WHERE pwrd = false
-    ),
-    -- Sum of deposits & withdrawals by week & wallet
-    gvt_acc AS (
-        SELECT week, 
-               wallet, 
-               SUM(CASE WHEN type = 'deposit' THEN gvt.amount END) as deposit,
-               SUM(CASE WHEN type = 'withdrawal' THEN gvt.amount END) as withdraw
-          FROM gvt
-          --WHERE wallet in ('\x00ff6b7d26407a46af2b631b4fa452a036d027e5','\x1c76fe4c0aac7b9d29aab6d17fd0f075e7fc84a5') --to check sample data
-        GROUP BY 1,2
-    ),
-    -- KPI calculations for New TVL, Renewed TVL & Churned TVL 
-    kpis AS (
-        SELECT period.week as "week",
-               gvt_acc.wallet as "wallet",
-               gvt_acc.deposit as "deposit_tvl",
-               gvt_acc.withdraw as "withdraw_tvl",
-               COALESCE(sum(gvt_acc_before.deposit), 0) AS "deposit_before", -- FOR INFO
-               CASE WHEN gvt_acc.deposit + COALESCE(sum(gvt_acc_before.deposit), 0) = gvt_acc.deposit
-                    THEN gvt_acc.deposit ELSE 0
-               END as "new_tvl",
-               CASE WHEN COALESCE(gvt_acc.deposit, 0) > 0 
-                    AND  COALESCE(sum(gvt_acc_before.deposit), 0) > 0
-                    THEN gvt_acc.deposit ELSE 0
-               END as "renewed_tvl",
-               COALESCE(gvt_acc.withdraw, 0) as "churned_tvl"
-        FROM period
-            LEFT JOIN gvt_acc
-            ON period.week = gvt_acc.week
-            LEFT JOIN gvt_acc gvt_acc_before
-            ON gvt_acc_before.week < period.week - interval '7 days'
-            AND gvt_acc.wallet = gvt_acc_before.wallet
-        GROUP BY 1,2,3,4
-        ),
-        -- KPI aggregations before calculating Retained TVL (can't use window with aggregate function)
-        kpis_aggr AS (
-            SELECT week,
-                   sum(deposit_tvl) AS deposit_tvl,
-                   sum(withdraw_tvl) AS withdraw_tvl,
-                   sum(new_tvl) AS new_tvl,
-                   sum(renewed_tvl) AS renewed_tvl,
-                   sum(churned_tvl) AS churned_tvl
-                   --retained_tvl
-            FROM kpis
-            GROUP BY 1--,7
-        ),
-        -- KPI calculation for Retained TVL
-        kpis_final AS (
-            SELECT week::date AS "week",
-                   --deposit_tvl AS "deposits",
-                   --withdraw_tvl AS "withdrawals",
-                   new_tvl AS "New TVL",
-                   renewed_tvl AS "Renewed TVL",
-                   -churned_tvl AS "Churned TVL",
-                   - COALESCE(deposit_tvl, 0)
-                   --+ COALESCE(withdraw_tvl ,0)
-                   --- COALESCE(churned_tvl, 0)
-                   + COALESCE(sum(deposit_tvl) OVER (ORDER BY week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW), 0)
-                   - COALESCE(sum(withdraw_tvl) OVER (ORDER BY week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW), 0)
-                   AS "Retained TVL",
-                 
-                  -- retained_tvl AS "Retained TVL",
-                   COALESCE(sum(deposit_tvl) OVER (ORDER BY week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW), 0)
-                   - COALESCE(sum(withdraw_tvl) OVER (ORDER BY week ASC ROWS BETWEEN unbounded preceding AND CURRENT ROW), 0) AS "Total TVL"
-            FROM kpis_aggr
-        ),
-        pnl_dates AS (
-            SELECT date_trunc('week', "evt_block_time") AS "week", 
-                max("evt_block_time") AS "max_time" 
-            FROM gro."PnL_evt_LogPnLExecution"
-            GROUP BY 1
-        ),
-        util_ratio AS (
-            SELECT "week" AS "week",
-                   max("gvt_tvl") AS "gvt_tvl",
-                   max("pwrd_tvl") AS "pwrd_tvl",
-                   max("total_tvl") AS "total_tvl",
-                   max("util") AS "Utilisation Ratio"
-            FROM (
-                SELECT pnl_dates.week AS "week",
-                       pnl."afterGvtAssets" / 1e18 as "gvt_tvl",
-                       pnl."afterPwrdAssets" / 1e18 as "pwrd_tvl",
-                       pnl."afterGvtAssets" / 1e18 + pnl."afterPwrdAssets" / 1e18 as "total_tvl",
-                       pnl."afterPwrdAssets" / pnl."afterGvtAssets" as "util"
-                FROM gro."PnL_evt_LogPnLExecution" pnl
-                INNER JOIN pnl_dates pnl_dates
-                     ON pnl.evt_block_time = pnl_dates.max_time
-            ) pnl
-            GROUP BY 1
-        )
-        
-SELECT kpis_final.*,
-       util_ratio."Utilisation Ratio"
-FROM kpis_final
-    LEFT JOIN util_ratio
-    ON kpis_final.week = util_ratio.week
-ORDER BY 1 ASC
-*/
--- 1. GRO - TVL acquisition, Retention, Churn/Totals.sql
+++ 1. GRO - TVL acquisition, Retention, Churn/Totals.sql
@@ -1,63 +1,88 @@
 /*
-/// @title  GRO - TVL
+/// @title  GRO - TVL v3
 /// @kpi    - Total TVL of GVT & PWRD
-/// @dev    - USD amounts based on deposit & withdrawal handler events, therefore excluding returns & rebasing (pwrd)
+/// @dev    - PWRD: USD amounts based on deposit & withdrawal handler events, therefore excluding returns & rebasing (pwrd)
+/// @dev    - GVT: USD amounts based on transfer amount * price deduced from PNL events
 */
 
 WITH
     -- Deposits & withdrawals (incl. all three versions of handlers)
-    handler AS (
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
+    pwrd AS (
+        SELECT date_trunc('week', evt_block_time) AS "week",
                "usdAmount" / 1e18 AS "amount",
                'deposit' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."DepositHandler_evt_LogNewDeposit"
         UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
+        SELECT date_trunc('week', evt_block_time)::date AS "week",
                "returnUsd" / 1e18 AS "amount",
                'withdrawal' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."WithdrawHandler_evt_LogNewWithdrawal"
          UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
+        SELECT date_trunc('week', evt_block_time) AS "week",
                "usdAmount" / 1e18 AS "amount",
                'deposit' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."DepositHandlerPrev1_evt_LogNewDeposit"
         UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
+        SELECT date_trunc('week', evt_block_time)::date AS "week",
                "returnUsd" / 1e18 AS "amount",
                'withdrawal' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."WithdrawHandlerPrev1_evt_LogNewWithdrawal"
          UNION ALL
-                 SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time) AS "week",
+        SELECT date_trunc('week', evt_block_time) AS "week",
                "usdAmount" / 1e18 AS "amount",
                'deposit' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."DepositHandlerPrev2_evt_LogNewDeposit"
         UNION ALL
-        SELECT evt_block_time AS "time",
-               date_trunc('week', evt_block_time)::date AS "week",
+        SELECT date_trunc('week', evt_block_time)::date AS "week",
                "returnUsd" / 1e18 AS "amount",
                'withdrawal' AS "type",
                "user" AS "wallet",
                "pwrd" AS "pwrd"
           FROM gro."WithdrawHandlerPrev2_evt_LogNewWithdrawal"
+    ),
+    -- Total GVT supply based on ERC20 transfers
+    gvt_total_supply AS (
+        SELECT
+            SUM(
+                CASE
+                    WHEN "from" = '\x0000000000000000000000000000000000000000' THEN value
+                    WHEN "to" = '\x0000000000000000000000000000000000000000' THEN -value
+                END
+            ) / 1e18 AS "total_supply"
+        FROM erc20."ERC20_evt_Transfer" tr
+        WHERE contract_address = '\x3ADb04E127b9C0a5D36094125669d4603AC52a0c'
+    ),
+    -- Total GVT assets based on PnL events
+    gvt_total_assets AS (
+        SELECT pnl."afterGvtAssets" / 1e18 as "gvt_assets"
+        FROM gro."PnL_evt_LogPnLExecution" pnl
+        ORDER BY evt_block_number DESC
+        LIMIT 1
+    ),
+    -- PWRD TVL
+    pwrd_tvl AS (
+        SELECT SUM(CASE WHEN pwrd = true THEN CASE WHEN type = 'deposit' THEN amount ELSE -amount END ELSE 0 END) AS "pwrd_tvl"
+        FROM pwrd
+    ),
+    -- GVT TVL
+    gvt_tvl AS (
+        SELECT total_supply * (gvt_assets / total_supply) as "gvt_tvl"
+        FROM gvt_total_supply,
+             gvt_total_assets
     )
 
-SELECT SUM(CASE WHEN type = 'deposit' THEN amount ELSE -amount END) AS "Total TVL",
-       SUM(CASE WHEN pwrd = true THEN CASE WHEN type = 'deposit' THEN amount ELSE -amount END ELSE 0 END) AS "PWRD TVL",
-       SUM(CASE WHEN pwrd = false THEN CASE WHEN type = 'deposit' THEN amount ELSE -amount END ELSE 0 END) AS "GVT TVL"
-FROM handler
-
+SELECT pwrd_tvl + gvt_tvl AS "Total TVL",
+       pwrd_tvl AS "PWRD TVL",
+       gvt_tvl AS "GVT TVL"
+FROM gvt_tvl,
+     pwrd_tvl
