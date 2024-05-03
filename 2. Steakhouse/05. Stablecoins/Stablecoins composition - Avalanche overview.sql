4e6eddfe42a45380c45a7fbd01bab5aebd9382a1
Sergi Juanati <wint3rmute@gmail.com>
2024-05-03 11:45:32 +0200

    stablecoins - avalanche

---
+++ 2. Steakhouse/05. Stablecoins/Stablecoins composition - Avalanche overview.sql
@@ -0,0 +1,2 @@
+SELECT *
+FROM "query_3678970(blockchain='avalanche_c', table='query_3578528')"
---
+++ 2. Steakhouse/05. Stablecoins/Stablecoins composition - Avalanche.sql
@@ -0,0 +1,157 @@
+WITH
+    asset_type AS (
+        SELECT asset, address
+        FROM (VALUES 
+            ('USDC', 0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e),
+            ('USDT', 0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7),
+            ('USP', 0xdacde03d7ab4d81feddc3a20faa89abac9072ce2),
+            ('DAI', 0xd586E7F844cEa2F87f50152665BCbc2C279D8d70)
+        ) AS t(asset, address)
+    ),
+    period_range AS (
+        SELECT asset, dt_seq, 0 AS amount, 0 AS volume, 0 AS txn_count
+        FROM asset_type
+        CROSS JOIN
+        UNNEST(SEQUENCE(DATE '2020-01-01', CURRENT_DATE, INTERVAL '1' DAY)) AS t(dt_seq)
+    ),
+    usdc AS (
+        SELECT
+            'USDC' as asset,
+            DATE(evt_block_time) AS dt,
+            SUM(
+                CASE
+                    WHEN "from" = 0x0000000000000000000000000000000000000000 THEN value
+                    WHEN "to" = 0x0000000000000000000000000000000000000000 THEN -value
+                END)
+            / 1e6 AS amount,
+            SUM(value) / 1e6 AS volume,
+            COUNT(*) AS txn_count
+        FROM circle_avalanche_c.FiatTokenV2_1_evt_Transfer
+        GROUP BY 1, 2
+    ),
+    usdt AS (
+        SELECT
+            'USDT' as asset,
+            DATE(evt_block_time) AS dt,
+            SUM(
+                CASE
+                    WHEN "from" = 0x0000000000000000000000000000000000000000 THEN value
+                    WHEN "to" = 0x0000000000000000000000000000000000000000 THEN -value
+                END)
+            / 1e6 AS amount,
+            SUM(value) / 1e6 AS volume,
+            COUNT(*) AS txn_count
+        FROM tether_avalanche_c.Tether_USD_evt_Transfer
+        GROUP BY 1, 2
+    ),
+    usp AS (
+        SELECT
+            'USP' as asset,
+            DATE(block_date) AS dt,
+            SUM(
+                CASE
+                    WHEN "from" = 0x0000000000000000000000000000000000000000 THEN amount_raw
+                    WHEN "to" = 0x0000000000000000000000000000000000000000 THEN -amount_raw
+                END)
+            / 1e18 AS amount,
+            SUM(amount_raw) / 1e18 AS volume,
+            COUNT(*) AS txn_count
+        FROM tokens_avalanche_c.transfers
+        WHERE contract_address = 0xdacde03d7ab4d81feddc3a20faa89abac9072ce2 -- USP
+        AND block_date > date '2022-12-05'
+        GROUP BY 1, 2
+    ),
+    dai AS (
+        SELECT
+            'DAI' as asset,
+            DATE(evt_block_time) AS dt,
+            SUM(
+                CASE
+                    WHEN "from" = 0x0000000000000000000000000000000000000000 THEN value
+                    WHEN "to" = 0x0000000000000000000000000000000000000000 THEN -value
+                END)
+            / 1e18 AS amount,
+            SUM(value) / 1e18 AS volume,
+            COUNT(*) AS txn_count
+        FROM maker_avalanche_c.BridgeToken_evt_Transfer
+        GROUP BY 1, 2
+    ),
+    combined_data AS (
+        SELECT
+            asset,
+            dt,
+            SUM(amount) AS amount,
+            SUM(volume) AS volume,
+            SUM(txn_count) AS txn_count
+        FROM (
+            SELECT * FROM usdc
+            UNION ALL
+            SELECT * FROM usdt
+            UNION ALL
+            SELECT * FROM usp
+            UNION ALL
+            SELECT * FROM dai
+            UNION ALL
+            SELECT * FROM period_range
+        )
+        GROUP BY 1, 2
+    ),
+    balances AS (
+        SELECT
+            COALESCE(at.address, 0x0000000000000000000000000000000000000000) AS address,
+            cd.asset,
+            cd.dt,
+            cd.txn_count AS txn_count,
+            cd.volume,
+            SUM(cd.amount) OVER (PARTITION BY cd.asset ORDER BY cd.dt) AS balance,
+            SUM(cd.amount) OVER (ORDER BY cd.dt) AS balance_total
+        FROM combined_data cd
+        LEFT JOIN asset_type at
+            ON cd.asset = at.asset
+    ),
+    balances_tt AS (
+        SELECT
+            address,
+            asset,
+            CONCAT(
+                '<a href="https://etherscan.io/token/',
+                CAST(address AS varchar),
+                '" target="_blank">🔗</a> ',
+                asset
+            ) AS asset_link,
+            dt,
+            txn_count,
+            balance,
+            balance_total,
+            volume,
+            COALESCE(LAG(volume, 7) OVER (PARTITION BY asset ORDER BY dt), 0) AS volume_7d_ago,
+            COALESCE(LAG(volume, 30) OVER (PARTITION BY asset ORDER BY dt), 0) AS volume_30d_ago,
+            CASE 
+                WHEN LAG(volume, 7) OVER (PARTITION BY asset ORDER BY dt) = 0 THEN 0
+                ELSE (volume - LAG(volume, 7) OVER (PARTITION BY asset ORDER BY dt))
+                     / LAG(volume, 7) OVER (PARTITION BY asset ORDER BY dt)
+            END AS volume_pct_7d,
+            CASE 
+                WHEN LAG(volume, 30) OVER (PARTITION BY asset ORDER BY dt) = 0 THEN 0
+                ELSE (volume - LAG(volume, 30) OVER (PARTITION BY asset ORDER BY dt))
+                     / LAG(volume, 30) OVER (PARTITION BY asset ORDER BY dt)
+            END AS volume_pct_30d,
+            COALESCE(LAG(balance, 7) OVER (PARTITION BY asset ORDER BY dt), 0) AS balance_7d_ago,
+            COALESCE(LAG(balance, 30) OVER (PARTITION BY asset ORDER BY dt), 0) AS balance_30d_ago,
+            CASE 
+                WHEN LAG(balance, 7) OVER (PARTITION BY asset ORDER BY dt) = 0 THEN 0
+                ELSE (balance - LAG(balance, 7) OVER (PARTITION BY asset ORDER BY dt))
+                     / LAG(balance, 7) OVER (PARTITION BY asset ORDER BY dt)
+            END AS balance_pct_7d,
+            CASE 
+                WHEN LAG(balance, 30) OVER (PARTITION BY asset ORDER BY dt) = 0 THEN 0
+                ELSE (balance - LAG(balance, 30) OVER (PARTITION BY asset ORDER BY dt))
+                     / LAG(balance, 30) OVER (PARTITION BY asset ORDER BY dt)
+            END AS balance_pct_30d
+        FROM balances
+    )
+    
+SELECT *
+FROM balances_tt
+WHERE dt > date '2020-01-01'
+ORDER BY dt ASC, balance DESC
